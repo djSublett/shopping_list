127d9c0f45427d1b795227cab78e5d1d009c9a7b
dsub502 <dsub502@yahoo.com>
2018-08-23 17:16:57 -0400

    created a shopping list in Python

--- shopping_list/shopping_list.py
+++ shopping_list/shopping_list.py
@@ -1,1 +1,39 @@
+shopping_list = []
 
+
+def show_help():
+    print("What should we pick up at the store?")
+    print("""
+Enter 'DONE' to stop adding items.
+Enter 'HELP' for this help.
+Enter 'SHOW' to see your current list.
+""")
+
+def add_to_list(item):
+    shopping_list.append(item)
+    print("Added! List has {} items.".format(len(shopping_list)))
+
+# new function named show_list that prints all items in the list
+def show_list():
+    print("Here is your list:")
+    for item in shopping_list:
+        print(item)
+
+show_help()
+while True:
+    new_item = input("> ")
+
+    if new_item == 'DONE':
+        break
+    elif new_item == 'HELP':
+        show_help()
+        continue
+   
+   #enable SHOW command to show list. Don't forget to update the HELP documentation. 
+    elif new_item == 'SHOW':
+        show_list()
+        continue
+
+    add_to_list(new_item)
+
+show_list()
